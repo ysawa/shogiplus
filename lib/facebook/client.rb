df5e75fbaa40f2510244a02d4f3944303c419b0a
Y Sawa <yoshihirosawa@gmail.com>
2011-10-15 14:38:21 +0900

    enable to get user picture easily

--- lib/facebook/client.rb
+++ lib/facebook/client.rb
@@ -6,9 +6,11 @@ require 'uri'
 
 class Facebook::Client
 
+  GRAPH_ROOT = "https://graph.facebook.com"
+
   # 1st. redirect to the url
   def authorize_url(scope = nil)
-    url = "https://graph.facebook.com/oauth/authorize?client_id=#{@app_id}&redirect_uri=#{URI.encode @redirect_uri}"
+    url = "#{GRAPH_ROOT}/oauth/authorize?client_id=#{@app_id}&redirect_uri=#{URI.encode @redirect_uri}"
     if scope.is_a?(Array)
       url += "&#{scope.join(',')}"
     end
@@ -19,7 +21,7 @@ class Facebook::Client
   def get_access_token_from_code(code)
     client = HTTPClient.new
     begin
-      url = "https://graph.facebook.com/oauth/access_token"
+      url = "#{GRAPH_ROOT}/oauth/access_token"
       params = { client_id: @app_id, redirect_uri: @redirect_uri, client_secret: @app_secret, code: code }
       result = client.get_content url, params
       pairs = result.split '&'
@@ -35,14 +37,17 @@ class Facebook::Client
 
   # 3rd. access to Graph API
   def get_graph(path = '/', params = {})
-    params.stringify_keys!
-    client = HTTPClient.new
+    response = get_graph_response(path, params)
     begin
-      url = "https://graph.facebook.com#{path.sub(/^(\/|)/, '/')}"
-      result = client.get_content url, params.merge({ access_token: @access_token })
-      return Facebook::Graph.parse result
+      return Facebook::Graph.parse response
     rescue
     end
+  end
+
+  def get_picture_url_from_user(user_graph)
+    user_id = user_graph.id
+    path = "/#{user_id}/picture"
+    "#{GRAPH_ROOT}#{path}"
   end
 
   def initialize(params)
@@ -51,5 +56,35 @@ class Facebook::Client
     @app_secret = params['app_secret']
     @access_token = params['access_token']
     @redirect_uri = params['redirect_uri']
+  end
+
+protected
+  def build_url(url, params = {})
+    if params.blank?
+      return url
+    else
+      if url.include? '?' # a little stinky
+        url_with_params = url + '&'
+      else
+        url_with_params = url
+      end
+      pairs = []
+      params.each do |key, value|
+        pairs << "#{key}=#{URI.encode value}"
+      end
+      url_with_params += pairs
+      url_with_params
+    end
+  end
+
+  def get_graph_response(path = '/', params = {})
+    params.stringify_keys!
+    client = HTTPClient.new
+    begin
+      url = "#{GRAPH_ROOT}#{path.sub(/^(\/|)/, '/')}"
+      result = client.get_content url, params.merge({ access_token: @access_token })
+      return result
+    rescue
+    end
   end
 end
